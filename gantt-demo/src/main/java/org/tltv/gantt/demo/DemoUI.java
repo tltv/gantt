c046150d998f972ff4849964b764736fe5dc33f6
Tomi Virtanen <tltv@vaadin.com>
2018-04-06 12:39:56 +0300

    Added timezone field to demo

--- gantt-demo/src/main/java/org/tltv/gantt/demo/DemoUI.java
+++ gantt-demo/src/main/java/org/tltv/gantt/demo/DemoUI.java
@@ -15,16 +15,23 @@
  */
 package org.tltv.gantt.demo;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.TextStyle;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.TimeZone;
 
 import org.tltv.gantt.Gantt;
 import org.tltv.gantt.model.Resolution;
-import org.tltv.gantt.model.Settings;
 
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.combobox.ComboBox;
+import com.vaadin.flow.component.datepicker.DatePicker;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.data.binder.Binder;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.theme.Theme;
 import com.vaadin.flow.theme.lumo.Lumo;
@@ -34,7 +41,7 @@ import com.vaadin.flow.theme.lumo.Lumo;
 @SuppressWarnings("serial")
 public class DemoUI extends Div {
 
-    private Binder<Settings> stateBinder;
+    private ZoneId defaultZoneId;
 
     public DemoUI() {
         setSizeFull();
@@ -44,30 +51,75 @@ public class DemoUI extends Div {
         gantt.setStartDateTime(LocalDateTime.of(2017, 1, 1, 0, 0, 0));
         gantt.setEndDateTime(LocalDateTime.of(2018, 1, 1, 0, 0, 0));
 
-        stateBinder = new Binder<>();
-
         final VerticalLayout layout = new VerticalLayout();
         layout.setClassName("demoContentLayout");
         layout.setMargin(false);
         layout.setSizeFull();
-        layout.add(createControls(stateBinder, gantt));
+        layout.add(createControls(gantt));
         layout.add(gantt);
 
         add(layout);
-
-        stateBinder.setBean(gantt.getSettings());
     }
 
-    private Div createControls(Binder<Settings> binder, final Gantt gantt) {
+    private Div createControls(final Gantt gantt) {
         Div controls = new Div();
 
-        ComboBox<Resolution> resolutionField = new ComboBox<>();
+        ComboBox<Resolution> resolutionField = new ComboBox<>("Resolution");
+        resolutionField.setPreventInvalidInput(true);
         resolutionField.setItems(Resolution.values());
         resolutionField.setValue(gantt.getResolution());
         resolutionField.addValueChangeListener(e -> gantt.setResolution(e.getValue()));
 
-        controls.add(resolutionField);
+        DatePicker startDateField = new DatePicker(
+                LocalDate.of(2017, 1, 1),
+                e -> Optional.ofNullable(e.getValue()).ifPresent(date -> gantt.setStartDateTime(date.atStartOfDay())));
+        startDateField.setLabel("Start date");
+
+        DatePicker endDateField = new DatePicker(
+                LocalDate.of(2018, 1, 1),
+                e -> Optional.ofNullable(e.getValue()).ifPresent(date -> gantt.setEndDateTime(date.atStartOfDay())));
+        endDateField.setLabel("End date");
+
+        ComboBox<String> timeZoneField = new ComboBox<>("Timezone", getSupportedTimeZoneIds());
+        timeZoneField.setWidth("350px");
+        timeZoneField.setValue("Default");
+        timeZoneField.setItemLabelGenerator(item -> {
+            if ("Default".equals(item)) {
+                return "Default (" + getDefaultTimeZone().getDisplayName(TextStyle.FULL, UI.getCurrent().getLocale())
+                        + ")";
+            }
+            TimeZone tz = TimeZone.getTimeZone(item);
+            return tz.getID() + " (raw offset " + (tz.getRawOffset() / 60000) + "m)";
+        });
+        timeZoneField.addValueChangeListener(e -> Optional.ofNullable(e.getValue()).ifPresent(zoneId -> {
+            if ("Default".equals(zoneId)) {
+                gantt.setZoneId(getDefaultTimeZone());
+            } else {
+                gantt.setZoneId(ZoneId.of(zoneId));
+            }
+        }));
+
+        controls.add(resolutionField, startDateField, endDateField, timeZoneField);
         return controls;
     }
 
+    private List<String> getSupportedTimeZoneIds() {
+        List<String> items = new ArrayList<>();
+        items.add("Default");
+        items.addAll(Gantt.getSupportedTimeZoneIDs());
+        return items;
+    }
+
+    private ZoneId getDefaultTimeZone() {
+        if (defaultZoneId != null) {
+            return defaultZoneId;
+        }
+        ZoneId zone = ZoneId.systemDefault();
+        if (Gantt.getSupportedTimeZoneIDs().contains(zone.getId())) {
+            defaultZoneId = zone;
+        } else {
+            defaultZoneId = ZoneId.of("Europe/Helsinki");
+        }
+        return defaultZoneId;
+    }
 }
